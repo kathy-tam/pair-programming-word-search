02e62f8b03cdc22ec1b2da07d1ca87355ce8d47b
Kathy <kathy.tam@mail.mcgill.ca>
2021-07-02 02:59:58 +0000

    Implemented word search horizontally and vertically, and added unit tests

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -20,7 +20,7 @@ describe("#wordSearch()", function() {
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
+  it("should return true if the word is present horizontally", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -34,5 +34,27 @@ describe("#wordSearch()", function() {
     ], 'SEINFELD')
 
     assert.isTrue(result);
+  });
+
+  it("should return true if the word is present vertically", function() {
+    const result = wordSearch([
+      ['A', 'W', 'S', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'E', 'N', 'F', 'E', 'J', 'D'],
+      ['Y', 'F', 'I', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'N', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'F', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'E', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'L', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'D', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'SEINFELD')
+
+    assert.isTrue(result);
+  });
+
+  it("should return false if matrix is empty", function() {
+    const result = wordSearch([], 'FRANK');
+
+    assert.isFalse(result);
   });
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,23 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const transpose = require('../../d2/matrix_transposition');
+
+const wordSearch = (letters, word) => {
+    // Check if matrix is empty
+    if(!letters.length) {
+        return false;
     }
+    // Horizontal Check
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    for (l of horizontalJoin) {
+        if (l.includes(word)) { return true }
+    }
+    // Vertical Check
+    const transposedLetters = transpose(letters);
+    const verticalJoin = transposedLetters.map(ls => ls.join(''));
+    for (l of verticalJoin) {
+        if (l.includes(word)) { return true }
+    }
+    // console.log(tranposedLetters);
+    return false;
 }
 
-module.exports = wordSearch
+module.exports = wordSearch;
